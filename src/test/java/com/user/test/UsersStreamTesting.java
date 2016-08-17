83a74a19614b744307f6827c656a6de0c653459d
Denis O <denis@universal-development.com>
2016-08-18 00:46:13 +0300

    Admin count reduction

--- src/test/java/com/user/test/UsersStreamTesting.java
+++ src/test/java/com/user/test/UsersStreamTesting.java
@@ -23,6 +23,7 @@ public class UsersStreamTesting {
         users  = Arrays.asList(
                 User.of(UserType.ADMIN, "admin@admin.com"),
                 User.of(UserType.ADMIN, "AdmiN2@aDmin.com"),
+                User.of(UserType.ADMIN, "POTATO@POTATO.com"),
                 User.of(UserType.USER, "user@user.com"),
                 User.of(UserType.USER, "user2@USER.CoM")
         );
@@ -62,6 +63,25 @@ public class UsersStreamTesting {
     public void mappingEmails() {
         // fetching logins into lower case
         users.stream().map(user -> user.getLogin()).map(String::toLowerCase).forEach(System.out::println);
+    }
+
+    //Reduce performs a reduction of the stream to a single element
+
+    @Test
+    public void countAdmins() {
+        Integer adminCount = users.stream().reduce(0, (count, user) -> {
+            System.out.format("accumulating count=%s; user=%s\n", count, user);
+            if (user.getType() == UserType.USER) {
+                return count;
+            }
+            return count + 1;
+        }, (count1, count2) -> {
+            System.out.format("combine %s; user=%s\n", count1, count2);
+            return count1 + count2;
+        });
+
+        System.out.println(adminCount);
+
     }
 
 }
