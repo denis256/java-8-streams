e49adad4aa2f3b21781792430eb17d579cf4cb71
Denis O <denis@universal-development.com>
2016-08-18 00:17:41 +0300

    User management service

---
+++ src/main/java/com/user/service/UserManager.java
@@ -0,0 +1,11 @@
+package com.user.service;
+
+import com.user.model.User;
+
+public class UserManager {
+
+    public void remove(User user) {
+        System.out.println("Removing user " + user);
+    }
+
+}
